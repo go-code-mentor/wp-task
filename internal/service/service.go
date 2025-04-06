fd47741db448de588e7a9d9408ddf7b9bb7832a1
Alexander Simonov <registsys@mail.ru>
2025-04-06 11:31:30 +0300

    [mod]: added task remover

--- internal/service/service.go
+++ internal/service/service.go
@@ -11,10 +11,22 @@ type StorageTaskGetter interface {
 	Task(ctx context.Context, id uint64) (entities.Task, error)
 }
 
+type StorageTaskRemover interface {
+	TaskRemove(ctx context.Context, id uint64) error
+}
+
 func Task(ctx context.Context, s StorageTaskGetter, id uint64) (entities.Task, error) {
 	task, err := s.Task(ctx, id)
 	if err != nil {
 		return task, fmt.Errorf("could not get task: %w", err)
 	}
 	return task, nil
+}
+
+func TaskRemove(ctx context.Context, s StorageTaskRemover, id uint64) error {
+	err := s.TaskRemove(ctx, id)
+	if err != nil {
+		return fmt.Errorf("could not remove task: %w", err)
+	}
+	return nil
 }
--- internal/service/service_test.go
+++ internal/service/service_test.go
@@ -21,6 +21,11 @@ func (m *MockedServices) Task(ctx context.Context, id uint64) (entities.Task, er
 	return args.Get(0).(entities.Task), args.Error(1)
 }
 
+func (m *MockedServices) TaskRemove(ctx context.Context, id uint64) error {
+	args := m.Called(id)
+	return args.Error(0)
+}
+
 func TestTaskGetting(t *testing.T) {
 
 	t.Run("success task getting", func(t *testing.T) {
@@ -52,6 +57,37 @@ func TestTaskGetting(t *testing.T) {
 		s.On("Task", taskId).Return(entities.Task{}, fmt.Errorf("error"))
 
 		_, err := service.Task(ctx, s, taskId)
+		s.AssertExpectations(t)
+
+		assert.Error(t, err)
+	})
+}
+
+func TestTaskRemoving(t *testing.T) {
+
+	t.Run("success task removing", func(t *testing.T) {
+
+		taskId := uint64(1)
+
+		ctx := context.Background()
+		s := new(MockedServices)
+		s.On("TaskRemove", taskId).Return(nil)
+
+		err := service.TaskRemove(ctx, s, taskId)
+
+		s.AssertExpectations(t)
+
+		assert.NoError(t, err)
+
+	})
+
+	t.Run("task removing with error", func(t *testing.T) {
+		taskId := uint64(1)
+		ctx := context.Background()
+		s := new(MockedServices)
+		s.On("TaskRemove", taskId).Return(fmt.Errorf("error"))
+
+		err := service.TaskRemove(ctx, s, taskId)
 		s.AssertExpectations(t)
 
 		assert.Error(t, err)
