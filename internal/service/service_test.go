f92faa5497c374f732e8644b7255d6153fb0839f
Alexander Simonov <registsys@mail.ru>
2025-04-06 11:07:54 +0300

    [mod]: added tasks getter

--- internal/service/service_test.go
+++ internal/service/service_test.go
@@ -21,6 +21,11 @@ func (m *MockedServices) Task(ctx context.Context, id uint64) (entities.Task, er
 	return args.Get(0).(entities.Task), args.Error(1)
 }
 
+func (m *MockedServices) Tasks(ctx context.Context) ([]entities.Task, error) {
+	args := m.Called()
+	return args.Get(0).([]entities.Task), args.Error(1)
+}
+
 func TestTaskGetting(t *testing.T) {
 
 	t.Run("success task getting", func(t *testing.T) {
@@ -56,4 +61,40 @@ func TestTaskGetting(t *testing.T) {
 
 		assert.Error(t, err)
 	})
+}
+
+func TestTaskыGetting(t *testing.T) {
+
+	t.Run("success tasks getting", func(t *testing.T) {
+		task := entities.Task{
+			ID:          1,
+			Name:        "Test task",
+			Description: "test task description",
+		}
+
+		ctx := context.Background()
+		s := new(MockedServices)
+		s.On("Tasks").Return([]entities.Task{task}, nil)
+
+		result, err := service.Tasks(ctx, s)
+
+		s.AssertExpectations(t)
+
+		assert.NoError(t, err)
+
+		assert.Equal(t, 1, len(result))
+		assert.Equal(t, task, result[0])
+	})
+
+	t.Run("tasks getting with error", func(t *testing.T) {
+		ctx := context.Background()
+		s := new(MockedServices)
+		s.On("Tasks").Return([]entities.Task{}, fmt.Errorf("error"))
+
+		_, err := service.Tasks(ctx, s)
+		s.AssertExpectations(t)
+
+		assert.Error(t, err)
+	})
+
 }
