34dcb5a9a08dbbb2c51a69e93dd0b1ad602c53f6
lks-go <alekksum@gmail.com>
2025-04-14 21:17:56 +0300

    app runner

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import (
+	"github.com/go-code-mentor/wp-task/internal/app"
+	"log"
+)
 
+func main() {
+	cfg, err := app.ParseConfig()
+	if err != nil {
+		log.Fatalf("failed to pasre config: %s", err)
+	}
+
+	a := app.New(cfg)
+
+	if err := a.Build(); err != nil {
+		log.Fatalf("failed to build app: %s", err)
+	}
+
+	if err := a.Run(); err != nil {
+		log.Fatalf("failed to run app: %s", err)
+	}
 }
