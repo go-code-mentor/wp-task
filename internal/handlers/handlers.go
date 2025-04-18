98f14523913a38d528513f1a81a07d78161142d1
lks-go <alekksum@gmail.com>
2025-04-18 21:27:19 +0300

    restore missed ItemHandler

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -26,6 +26,21 @@ type TasksHandler struct {
 	Service Service
 }
 
+func (h *TasksHandler) ItemHandler(c *fiber.Ctx) error {
+
+	taskId, err := strconv.Atoi(c.Params("id"))
+	if err != nil {
+		return fiber.ErrNotFound
+	}
+
+	task, err := h.Service.Task(c.Context(), uint64(taskId))
+	if err != nil {
+		return fiber.ErrNotFound
+	}
+
+	return c.JSON(task)
+}
+
 func (h *TasksHandler) ListHandler(c *fiber.Ctx) error {
 
 	tasks, err := h.Service.Tasks(c.Context())
