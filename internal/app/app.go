bfcf52d3c9f5813559668638589e41eed7d7da48
Alexander Simonov <registsys@mail.ru>
2025-04-19 12:58:28 +0300

    [mod]: added db connection

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,9 +1,14 @@
 package app
 
 import (
+	"database/sql"
+	"fmt"
+
+	"github.com/gofiber/fiber/v2"
+	_ "github.com/lib/pq"
+
 	"github.com/go-code-mentor/wp-task/internal/handlers"
 	"github.com/go-code-mentor/wp-task/internal/service"
-	"github.com/gofiber/fiber/v2"
 )
 
 func New(cfg Config) *App {
@@ -15,9 +20,14 @@ func New(cfg Config) *App {
 type App struct {
 	cfg    Config
 	server *fiber.App
+	db     *sql.DB
 }
 
 func (a *App) Build() error {
+
+	if err := a.connectDb(); err != nil {
+		return err
+	}
 
 	a.server = fiber.New()
 
@@ -35,4 +45,20 @@ func (a *App) Build() error {
 
 func (a *App) Run() error {
 	return a.server.Listen(":3000")
+}
+
+func (a *App) connectDb() error {
+
+	db, err := sql.Open("postgres", a.cfg.pg_uri)
+	if err != nil {
+		return fmt.Errorf("could not connect db: %w", err)
+	}
+
+	if err := db.Ping(); err != nil {
+		return fmt.Errorf("could not connect db: %w", err)
+	}
+
+	a.db = db
+
+	return nil
 }
--- internal/app/config.go
+++ internal/app/config.go
@@ -1,8 +1,30 @@
 package app
 
+import (
+	"fmt"
+	"os"
+)
+
 func ParseConfig() (Config, error) {
-	return Config{}, nil
+	cfg := Config{}
+
+	cfg.parseDb()
+
+	return cfg, nil
 }
 
 type Config struct {
+	pg_uri string
+}
+
+func (c *Config) parseDb() {
+	host := os.Getenv("POSTGRES_HOST")
+	db_name := os.Getenv("POSTGRES_DB")
+	user := os.Getenv("POSTGRES_USER")
+	pass := os.Getenv("POSTGRES_PASSWORD")
+	port := os.Getenv("POSTGRES_PORT")
+	if port == "" {
+		port = "5432"
+	}
+	c.pg_uri = fmt.Sprintf("postgresql://%s:%s@%s:%s/%s?sslmode=disable", user, pass, host, port, db_name)
 }
