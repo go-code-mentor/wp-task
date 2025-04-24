93f01da8a167238d5c2f790fca0e653c8d5ffee9
Dmitry Tumalanov <tdmitry@fibex.su>
2025-04-24 15:42:58 +0300

    storage: add tasks method

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -2,18 +2,28 @@ package storage
 
 import (
 	"context"
+	"fmt"
 	"github.com/go-code-mentor/wp-task/internal/entities"
 	"github.com/jackc/pgx/v5"
+	"time"
 )
 
+const rowsRetrieveTimeout = 10 * time.Second
+
 type Storage struct {
-	pool *pgx.Conn
+	conn *pgx.Conn
 }
 
-func New(pool *pgx.Conn) *Storage {
+func New(conn *pgx.Conn) *Storage {
 	return &Storage{
-		pool: pool,
+		conn: conn,
 	}
+}
+
+type TaskSQL struct {
+	ID          uint64 `db:"id"`
+	Name        string `db:"name"`
+	Description string `db:"description"`
 }
 
 func (s *Storage) Task(ctx context.Context, id uint64) (entities.Task, error) {
@@ -21,7 +31,35 @@ func (s *Storage) Task(ctx context.Context, id uint64) (entities.Task, error) {
 }
 
 func (s *Storage) Tasks(ctx context.Context) ([]entities.Task, error) {
-	return []entities.Task{}, nil
+	// Create context with timeout for SQL query
+	c, cancel := context.WithTimeout(ctx, rowsRetrieveTimeout)
+	defer cancel()
+
+	// Run SQL query
+	query := `SELECT id, name, description FROM tasks`
+	rows, err := s.conn.Query(c, query)
+	if err != nil {
+		return nil, fmt.Errorf("unbale to get query tasks from storage: %w", err)
+	}
+
+	// Parse SQL query to DTO
+	tasksSQL := make([]TaskSQL, 0)
+	tasksSQL, err = pgx.CollectRows(rows, pgx.RowToStructByName[TaskSQL])
+	if err != nil {
+		return nil, fmt.Errorf("unbale to get query tasks from storage: %w", err)
+	}
+
+	// Convert DTO to entity
+	tasks := make([]entities.Task, len(tasksSQL))
+	for i := range tasksSQL {
+		tasks = append(tasks, entities.Task{
+			ID:          tasksSQL[i].ID,
+			Name:        tasksSQL[i].Name,
+			Description: tasksSQL[i].Description,
+		})
+	}
+
+	return tasks, nil
 }
 
 func (s *Storage) TaskRemove(ctx context.Context, id uint64) error {
