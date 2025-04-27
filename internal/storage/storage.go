07366289228678a99e5772bc6e2d181f124fa1ba
Dmitry Tumalanov <tumalanov37@gmail.com>
2025-04-27 12:10:56 +0300

    storage: TaskAdd : remove created_at, updated_at from sql query

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -83,7 +83,7 @@ func (s *Storage) TaskAdd(ctx context.Context, task entities.Task) error {
 	}
 
 	// Run SQL query
-	query := "INSERT INTO tasks (name, description, created_at, updated_at) VALUES ($1, $2, now(), now())"
+	query := "INSERT INTO tasks (name, description) VALUES ($1, $2)"
 	_, err := s.conn.Exec(c, query, taskSQL.Name, taskSQL.Description)
 	if err != nil {
 		return fmt.Errorf("unable to add task to storage: %w", err)
