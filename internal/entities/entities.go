217392480231250896d352da7edb6c22106567d9
Alexander Simonov <registsys@mail.ru>
2025-04-30 22:18:03 +0300

    [fix]: removed unused models

--- internal/entities/entities.go
+++ internal/entities/entities.go
@@ -6,14 +6,3 @@ type Task struct {
 	Description string
 	Owner       string
 }
-
-type User struct {
-	ID    uint64
-	Login string
-}
-
-type AccessToken struct {
-	ID     uint64
-	UserId uint64
-	Token  string
-}
