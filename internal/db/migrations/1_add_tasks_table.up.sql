8be887186f6131f181db310e8f87ddc76e36aa9b
Alexander Simonov <registsys@mail.ru>
2025-04-30 22:46:58 +0300

    [fix]: drop migrations

--- internal/db/migrations/1_add_tasks_table.up.sql
+++
@@ -1,9 +0,0 @@
-create table if not exists public.tasks
-(
-    id BIGSERIAL primary key,
-    name text not null,
-    description text,
-    created_at TIMESTAMPTZ DEFAULT now(),
-    updated_at TIMESTAMPTZ DEFAULT now()
-);
-
